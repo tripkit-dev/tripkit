591cb7efd5c382e47555dfe8db0270f90b9df521
송치훈 <marhead@songchihoon-ui-MacBookPro.local>
2023-01-23 12:50:39 +0900

    Update .gitignore

--- src/test/java/com/tripkit/lighthouse/service/SpotServiceTest.java
+++ src/test/java/com/tripkit/lighthouse/service/SpotServiceTest.java
@@ -34,30 +34,30 @@ class SpotServiceTest {
     @DisplayName("Title로 위치정보 가져오기")
     void getSpotByTitle() {
         // API 설정
-        String googlePlaceAPI = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=";
-        String mainQuery = "카페";
-        String optionalFields = "name,rating,opening_hours,geometry,place_id";
-        String APIkey = googleAPI.getKey();
-
-        String url = googlePlaceAPI + mainQuery + "&inputtype=textquery&fields=" + optionalFields + "&key=" + APIkey;
-
-        // API 요청 빌드 시작 부분
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "");
-        Request request = new Request.Builder()
-                .url(url)
-//                .method("GET", body)
-                .build();
-        try(Response response = client.newCall(request).execute()) {
-            System.out.println(response);
-            System.out.println(response.body());
-
-            response.body();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        String googlePlaceAPI = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=";
+//        String mainQuery = "카페";
+//        String optionalFields = "name,rating,opening_hours,geometry,place_id";
+//        String APIkey = googleAPI.getKey();
+//
+//        String url = googlePlaceAPI + mainQuery + "&inputtype=textquery&fields=" + optionalFields + "&key=" + APIkey;
+//
+//        // API 요청 빌드 시작 부분
+//        OkHttpClient client = new OkHttpClient().newBuilder()
+//                .build();
+//        MediaType mediaType = MediaType.parse("text/plain");
+//        RequestBody body = RequestBody.create(mediaType, "");
+//        Request request = new Request.Builder()
+//                .url(url)
+////                .method("GET", body)
+//                .build();
+//        try(Response response = client.newCall(request).execute()) {
+//            System.out.println(response);
+//            System.out.println(response.body());
+//
+//            response.body();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
     @Test
